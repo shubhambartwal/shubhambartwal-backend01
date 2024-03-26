8e2080362ef580f5621fad2c86bc716752093d1d
shubhambartwal <shubhambartwal58@gmail.com>
2024-03-26 22:22:30 +0530

    initial commit

---
+++ middleware/index.js
@@ -0,0 +1,15 @@
+const express= require('express')
+const app=express();
+function isOldEnough(age){
+    age>=14?true:false
+    }
+app.get('/ride1',function(req,res){
+    if(isOldEnough(req.query.age)){
+   console.log("ride 1 is working fine")
+    res.json({msg:"you have successfull riden the ride 1"})
+    }
+    else{
+       res.send(411).json({msg:'sorry you cant ride'}) 
+    }
+})
+app.listen(3000,()=>{console.log('working fine')})
