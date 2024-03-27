dbbd6eb1f6a0c9eb6ce3318935f011790cf83ce3
shubhambartwal <shubhambartwal58@gmail.com>
2024-03-27 11:00:17 +0530

    midleware basics

--- middleware/index.js
+++ middleware/index.js
@@ -1,15 +1,40 @@
 const express= require('express')
 const app=express();
 function isOldEnough(age){
-    age>=14?true:false
+   return age>=14?true:false
     }
-app.get('/ride1',function(req,res){
-    if(isOldEnough(req.query.age)){
-   console.log("ride 1 is working fine")
+    function isOldEnoughMiddleware(req,res,next){
+        const age= req.query.age
+        if(age>14){next()}
+        else res.json({msg:"You are underage"})
+    }
+app.get('/ride1',function(req,res)
+{
+     if(isOldEnough(req.query.age)){
+    console.log("ride 1 is working fine")
     res.json({msg:"you have successfull riden the ride 1"})
     }
     else{
-       res.send(411).json({msg:'sorry you cant ride'}) 
+       res.json({msg:'sorry you cant ride'}) 
+    
     }
+}
+)
+app.get('/ride2',function(req,res)
+{
+     if(isOldEnough(req.query.age)){
+    console.log("ride 1 is working fine")
+    res.json({msg:"you have successfull riden the ride 2"})
+    }
+    else{
+       res.status(411).json({msg:'sorry you cant ride2'}) 
+    
+    }
+})
+
+//middleware function 
+app.get('/ride3',isOldEnoughMiddleware,function(req,res)
+{res.json({msg:"you have successfull riden the ride 3"})
+    
 })
 app.listen(3000,()=>{console.log('working fine')}) 
