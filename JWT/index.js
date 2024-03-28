7d1c5341f95dcd059a6a7fb4cea7ef535a7b2390
shubhambartwal <shubhambartwal58@gmail.com>
2024-03-28 21:44:24 +0530

    jwt

--- JWT/index.js
+++ JWT/index.js
@@ -11,4 +11,15 @@ const token=jwt.sign(value,"secret");
 //this token has been generated using the secret and hec=nce this token 
 // can only be verified using this secret
 
+function decode(token)
+{
+    console.log(jwt.decode(token))
+}
 console.log(token)
+
+function verify(token ,secret="secret"){
+jwt.verify(token,secret)
+console.log('wokring')
+}
+decode(token)
+verify(token,"jdydyrs")
