7bc39e767893ddacb6cb36628022034ab99d988d
shubhambartwal <shubhambartwal58@gmail.com>
2024-04-01 21:59:07 +0530

    middleware

--- Mongoose/Middleware/adminMiddleware.js
+++ Mongoose/Middleware/adminMiddleware.js
@@ -1,4 +1,14 @@
+const {Admin}=require('../DB/Admin.model')
 const adminMiddleware=(req,res,next)=>{
-next();
+Admin.findOne({
+    username:req.header.username,
+    password:req.header.password
+}).then(function (value){
+    if(value){
+        next();
+    }else{
+        res.status(403).json({msg:"user doesn't exist"})
+    }
+})
 }
 module.exports=adminMiddleware;
--- Mongoose/Routes/AdminRoute.js
+++ Mongoose/Routes/AdminRoute.js
@@ -1,17 +1,24 @@
-const {Router} = require('express')
-const adminMiddleware = require('../Middleware/adminMiddleware')
-const router= Router();
+const { Router } = require("express");
+const adminMiddleware = require("../Middleware/adminMiddleware");
+const Admin = require("../DB/Admin.model");
+const router = Router();
 // Admin Routes
-router.post('/signup', (req, res) => {
-    // Implement admin signup logic
+router.post("/signup", (req, res) => {
+  // Implement admin signup logic
+  Admin.create({
+    username: username,
+    password: password,
+  }).then(function () {
+    res.json({ message: "Admin created successfully" });
+  });
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
-    // Implement course creation logic
+router.post("/courses", adminMiddleware, (req, res) => {
+  // Implement course creation logic
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
-    // Implement fetching all courses logic
+router.get("/courses", adminMiddleware, (req, res) => {
+  // Implement fetching all courses logic
 });
 
 module.exports = router;
