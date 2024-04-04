02a2561b426d2ac2e99b534d84052eb0384b320a
shubhambartwal <shubhambartwal58@gmail.com>
2024-04-04 23:43:29 +0530

    small changes

--- Mongoose/DB/index.js
+++ Mongoose/DB/index.js
@@ -1,2 +1,8 @@
 const mongoose= require('mongoose')
-mongoose.connect()
+function DbConnection(){
+mongoose.connect("mongodb+srv://test:test@cluster0.k4a1v8n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+function(){
+    console.log('connected')
+})
+}
+module.exports=DbConnection
--- Mongoose/index.js
+++ Mongoose/index.js
@@ -1,7 +1,9 @@
 const express= require('express')
 const AdminRoute =require('./Routes/AdminRoute')
-const UserRoute= require('./Routes/UserRoute')
+const UserRoute= require('./Routes/UserRoute');
+const DbConnection = require('./DB');
 const app= express();
+app.use(DbConnection);
 app.use('/admin',AdminRoute )
 app.use('/user',UserRoute)
 app.listen(3000,console.log(
